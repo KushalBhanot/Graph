017bcfbcfe04e068ac3d9c6f44a3028666e4c94c
Kushal Bhanot <developerkushalb@gmail.com>
2021-12-09 11:56:08 +0530

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int start, vector<vector<int>>& adj, vector<bool>& vis) {
+    vis[start] = true;
+    
+    for(int next_node: adj[start]) {
+        if(!vis[next_node])
+            dfs(next_node, adj, vis);
+    }
+    
+    cout << start + 1 << " ";
+}
+
+int main() {
+    int v, e;
+    cin >> v >> e;
+    
+    vector<vector<int>> adj(v);
+    for(int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
+        
+        // undirected graph
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    int start = 0;
+    cin >> start;
+    
+    vector<bool> vis(v, false);
+    dfs(start, adj, vis);
+}
